ecd4989a2b5a6119aa8cef489b8c8c06d935bd05
Maija Hoikka <hoikka.maija@gmail.com>
2019-12-09 22:43:33 +0200

    crud metodit

--- express.js
+++ express.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors')
+//const bodyParser = require('body-parser')
+
+//app.use(bodyParser.json())
 
 const router = require("./router");
 
--- router.js
+++ router.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const todos = require("./todos");
+
+let todos = [{
+        id: 1,
+        title: "Koulu",
+        description: "Tee läksyt",
+        due: "Aina kun niitä tulee"
+    },
+    {
+        id: 2,
+        title: "Kauppa",
+        description: "Osta maitoa",
+        due: "Kun kerkiät"
+    },
+    {
+        id: 3,
+        title: "Työt",
+        description: "Mene töihin",
+        due: "Arki aamuisin"
+    }
+]
 
 router.get("/todos", (request, response) => {
     response.status(200).send(todos);
@@ -13,10 +32,79 @@ router.get("/todos/:id", (request, response) => {
 
     if (todo) {
         response.send(todo);
-    }
-    else { 
+    } else {
         response.status(404).send("Muistiinpanoa ei löytynyt");
     }
+});
+
+
+const generateId = () => {
+
+    const maxId = todos.length > 0 ?
+        Math.max(...todos.map(n => n.id)) :
+        0
+    return maxId + 1
+}
+
+router.post('/todos', (request, response) => {
+
+    const body = request.body;
+
+    if (!body.title || !body.due) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const todo = {
+        id: generateId(),
+        title: body.title,
+        description: body.description,
+        due: body.due,
+    }
+
+    todos = todos.concat(todo);
+
+    response.json(todo);
+});
+
+router.put("/todos/:id", (request, response) => {
+
+    const body = request.body;
+    const id = Number(request.params.id);
+    const todo = todos.find(todo => todo.id === id);
+
+    if (!todo) {
+        const todo = {
+            id: generateId(),
+            title: body.title,
+            description: body.description,
+            due: body.due,
+
+        }
+
+        todos = todos.concat(todo);
+
+        response.json(todo);
+    } else {
+        todo = {
+            id: id,
+            title: body.title,
+            description: body.description,
+            due: body.due,
+        }
+
+        todos = todos.splice(todo);
+
+        response.json(todo);
+    }
+});
+
+router.delete('/todos/:id', (request, response) => {
+    const id = Number(request.params.id)
+    todos = todos.filter(todo => todo.id !== id)
+
+    response.status(204).end();
 });
 
 module.exports = router;
